ee4195a2ec4790f850f0e0da9cc4f4746856d6d6
150709039 <ramazanayoz2@posta.mu.edu.tr>
2021-05-14 13:56:56 +0300

    sec 6 - 25. Response Entity & HTTP Response Codes

--- src/main/java/com/ramazanayoz/learningspringbootramazanayoz/resource_otherName_api_otherName_controller/UserResource.java
+++ src/main/java/com/ramazanayoz/learningspringbootramazanayoz/resource_otherName_api_otherName_controller/UserResource.java
@@ -1,9 +1,12 @@
 package com.ramazanayoz.learningspringbootramazanayoz.resource_otherName_api_otherName_controller;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -29,7 +32,28 @@ public class UserResource {
 	}
 	
 	@RequestMapping(method = RequestMethod.GET, path = "{userUid}")
-	public User fetchUser(@PathVariable("userUid") UUID useUid) {
-		return userService.getUser(useUid).get();
+	public ResponseEntity<?> fetchUser(@PathVariable("userUid") UUID userUid) {
+		Optional<User> userOptional = userService.getUser(userUid);
+		if(userOptional.isPresent()) {
+			return ResponseEntity.ok(userOptional.get());
+		}
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(new ErrorMessage("user"+ userUid+ " was not found."));
+	}
+	
+	class ErrorMessage {
+		String errorMessage;
+		
+		public ErrorMessage(String errorMessage) {
+			this.errorMessage = errorMessage;
+		}
+		
+		public String getErrorMessage() {
+			return errorMessage;
+		}
+		
+		public void setErrorMessage(String errorMessage) {
+			this.errorMessage = errorMessage;
+		}
 	}
 }
