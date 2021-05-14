7f15ab93207f99f45007cee880b77deec3a60ce0
150709039 <ramazanayoz2@posta.mu.edu.tr>
2021-05-14 13:24:30 +0300

    section6 - 22. HTTP GET Requets

---
+++ src/main/java/com/ramazanayoz/learningspringbootramazanayoz/resource_otherName_api_otherName_controller/UserResource.java
@@ -0,0 +1,27 @@
+package com.ramazanayoz.learningspringbootramazanayoz.resource_otherName_api_otherName_controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ramazanayoz.learningspringbootramazanayoz.model.User;
+import com.ramazanayoz.learningspringbootramazanayoz.service.UserService;
+
+@RestController
+public class UserResource {
+
+	private UserService userService;
+	
+	@Autowired
+	public UserResource(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public List<User> fetUsers(){
+		return userService.getAllUsers();
+	}
+}
